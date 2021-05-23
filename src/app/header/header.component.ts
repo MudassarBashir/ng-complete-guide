23e2a6cf37d0292587d0f1f4aa93c0aee830dadb
Mudassar "Moe" Bashir <moeb59@gmail.com>
2021-05-23 08:28:50 -0400

    Finishing up components and data binding.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  @Output() headerItemSelected = new EventEmitter<string>();
+
+  onHeaderItemSelected(headerItem: string): void {
+    this.headerItemSelected.emit(headerItem);
+  }
+}
