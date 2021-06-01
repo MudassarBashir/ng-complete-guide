757feeb6ccd707a13c69ff783b0df681a0ebd890
Mudassar "Moe" Bashir <moeb59@gmail.com>
2021-06-01 15:40:54 -0400

    Using a service for cross-component communication.

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,3 +1,4 @@
+import { RecipesService } from './recipes.services';
 import { Component, OnInit } from '@angular/core';
 
 import { Recipe } from './recipe.model';
@@ -6,11 +7,16 @@ import { Recipe } from './recipe.model';
     selector: 'app-recipes',
     templateUrl: './recipes.component.html',
     styleUrls: ['./recipes.component.css'],
+    providers: [RecipesService],
 })
 export class RecipesComponent implements OnInit {
     selectedRecipe: Recipe;
 
-    constructor() {}
+    constructor(private recipesService: RecipesService) {}
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.recipesService.recipeSelected.subscribe((recipe: Recipe) => {
+            this.selectedRecipe = recipe;
+        });
+    }
 }
